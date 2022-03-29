9304e78e6a7906fc63031c9887bd6310fa15aeda
Pashlk <hghgjk.ru@gmail.com>
2022-03-29 18:16:46 +0300

    2.14

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-let  i = 1;
-while (true) {
-    let num = prompt('Введите число', '');
-    if (num <= 100) {
-        alert(num)
-    } else if (!num || num > 100) {
-        break
-    }
+const number = +prompt('введите число от 1 до 3', '');
+switch (number) {
+    case 1 :
+        alert('Вы ввели число 1');
+        break;
+    case 2 :
+        alert('Вы ввели число 2');
+        break;
+    case 3 :
+        alert('Вы ввели число 3');
+        break;
+    default :
+        alert('Научитесь читать условие и попробуйте еще разок');
+        break;
 }
+
